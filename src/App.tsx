76b767a3c7a2ed1e7288d95bda6c41c4f7f9d24a
Leru Xie <1486835097@qq.com>
2020-06-02 23:36:29 +0800

    feat: Button组件基本完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button disabled >Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>Hello</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Hello</Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com">Hello</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
---
+++ src/components/Button/button.tsx
@@ -0,0 +1,73 @@
+import React from 'react'
+import classNames from 'classnames'
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+interface BaseButtonProps {
+  className?: string,
+  disabled?: boolean,
+  size?: ButtonSize,
+  btnType?: ButtonType,
+  children?: React.ReactNode,
+  href ?: string,
+}
+
+type NativeButtonProps = React.ButtonHTMLAttributes<HTMLElement> & BaseButtonProps
+type AnchorButtonProps = React.AnchorHTMLAttributes<HTMLElement> & BaseButtonProps
+
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+const Button: React.FC<ButtonProps> = (props) => {
+  const { 
+    btnType,
+    disabled,
+    size,
+    children,
+    className,
+    href,
+    ...restProps
+  } = props
+  const classes = classNames('btn', className,  {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === ButtonType.Link) && disabled
+  })
+  if (btnType === ButtonType.Link && href) {
+    return (
+      <a
+        {...restProps}
+        className={classes}
+        href={href}
+      >
+        {children}
+      </a>
+    )
+  } else  {
+    return (
+      <button
+        {...restProps}
+        className={classes}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button
